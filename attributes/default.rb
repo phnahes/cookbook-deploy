2a0499fc64457746ade79fb28cbb020d9fecff9f
Paulo Nahes <phnahes@gmail.com>
2013-10-24 16:16:40 -0200

    Updating way that cookbook receive the databag and packages

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,9 +7,14 @@
 default[:deploy]["env1"] 	= "homol"
 default[:deploy]["env2"] 	= "prod"
 
-# Bags and Packages List
-default[:deploy]["bag"] 	= ""
-default[:deploy]["pkgList"] 	= [ "" ]
-
 # Shell Command to actuate on services
 default[:deploy]["cmd"] 	= "/usr/bin/service"
+
+# Bags and Packages List
+default[:deploy]['install'] = { }
+
+#default[:deploy]['install'] = { 
+#	'deploy-db1' => ["db1par1", "db1par2", "db1par3"],
+#	'deploy-db2' => ["db2par1", "db2par2", "db2par3"],
+#	'deploy-db3' => ["db3par1", "db3par2", "db3par3"]
+#}
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,56 +3,58 @@
 # Recipe: default
 # 
 
-bag  	= node[:deploy]["bag"]
-pkgList = node[:deploy]["pkgList"]
 cmd  	= node[:deploy]["cmd"]
 hash 	= Hash.new
 
 env1	= node[:deploy]["env1"]
 env2	= node[:deploy]["env2"]
 
-pkgList.each do |packageName|
-	pkgLoop = data_bag_item(bag, packageName)
+node[:deploy]['install'].each do |bag, pkgs|
+	log "DataBag: #{bag}"
+	pkgs.each do |packageName|
+		log "DataBag: #{bag} :: Pacote: #{packageName}"
+		pkgLoop = data_bag_item(bag,packageName)
 
-	if pkgLoop
-		pkg_env1 	= pkgLoop["#{env1}"]["version"]
-		pkg_env2  	= pkgLoop["#{env2}"]["version"]
-		
-		pkg_name = pkgLoop["id"]
+		if pkgLoop
+			pkg_env1 	= pkgLoop["#{env1}"]["version"]
+			pkg_env2  	= pkgLoop["#{env2}"]["version"]
+			
+			pkg_name = pkgLoop["id"]
 
-		case node.chef_environment
-		when "#{env1}" # Environment 1
-			pkg_version = pkg_env1
-		when "#{env2}"  # Environment 2
-			pkg_version = pkg_env2
-		end
+			case node.chef_environment
+			when "#{env1}" # Environment 1
+				pkg_version = pkg_env1
+			when "#{env2}"  # Environment 2
+				pkg_version = pkg_env2
+			end
 
-		if defined? pkg_version
-			if not pkg_version.empty?
+			if defined? pkg_version
+				if not pkg_version.empty?
 
-				ruby_block "queueService" do
-					block do
-						pkg_service_name 	= pkgLoop["service"]["name"]
-						pkg_service_action 	= pkgLoop["service"]["action"]
+					ruby_block "queueService" do
+						block do
+							pkg_service_name 	= pkgLoop["service"]["name"]
+							pkg_service_action 	= pkgLoop["service"]["action"]
 
-						if defined? pkg_service_name
-							if not pkg_service_name.empty?
-								hash.merge! pkg_service_name => pkg_service_action
+							if defined? pkg_service_name
+								if not pkg_service_name.empty?
+									#log "Schedulling Service #{pkg_service_name} to #{pkg_service_action} ..."
+									hash.merge! pkg_service_name => pkg_service_action
+								end
 							end
 						end
+						action :nothing
+					end				
+					package "#{pkg_name}" do
+						version "#{pkg_version}"
+						options "--force-yes"
+						ignore_failure true
+						notifies :create, "ruby_block[queueService]", :immediately
 					end
-					action :nothing
-				end				
 
-				package "#{pkg_name}" do
-					version "#{pkg_version}"
-					options "--force-yes"
-					ignore_failure true
-					notifies :create, "ruby_block[queueService]", :immediately
 				end
-
 			end
-		end
+		end			
 	end
 end
 
